4073543cfacb4e42c72c53098dfc782b0f352896
Peter Ahinko <peter@ahinko.se>
2021-04-26 22:13:04 +0200

    Prevent Tigera operator from being scheduled on Atlas

--- tools/bootstrap/update-tigera-operator-manifest.sh
+++ tools/bootstrap/update-tigera-operator-manifest.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 wget https://docs.projectcalico.org/manifests/tigera-operator.yaml -O cluster/tigera-operator/operator/tigera-operator-crds.yaml
+
+cat <<EOT >> cluster/tigera-operator/operator/tigera-operator-crds.yaml
+      affinity:
+        nodeAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+            nodeSelectorTerms:
+              - matchExpressions:
+                  - key: kubernetes.io/hostname
+                    operator: In
+                    values:
+                      - helios
+                      - zeus
+                      - iris
+EOT
