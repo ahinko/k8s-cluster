9a02c043199c6ec4a2efc72ce5842233180ec846
Peter Ahinko <peter@ahinko.se>
2021-05-15 18:27:19 +0200

    Update CRDs

--- tools/bootstrap/update-bootstrap-crds.sh
+++ tools/bootstrap/update-bootstrap-crds.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CERT_MANAGER_VERSION=v1.2.0
-VAULT_SECRETS_OPERATOR_VERSION=1.13.0
+CERT_MANAGER_VERSION=v1.3.1
+VAULT_SECRETS_OPERATOR_VERSION=1.14.3
 
 # Get CRDs for Cert Manager
-wget https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.crds.yaml -O bootstrap/crds/cert-manager-crds.yaml
+wget https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.crds.yaml -O cluster/cert-manager/cert-manager/crds.yaml
 
 # Get CRDs for Vault Secrets Operator
-wget https://raw.githubusercontent.com/ricoberger/vault-secrets-operator/${VAULT_SECRETS_OPERATOR_VERSION}/config/crd/bases/ricoberger.de_vaultsecrets.yaml -O bootstrap/crds/vault-secrets-operator-crds.yaml
+wget https://raw.githubusercontent.com/ricoberger/vault-secrets-operator/${VAULT_SECRETS_OPERATOR_VERSION}/config/crd/bases/ricoberger.de_vaultsecrets.yaml -O cluster/vault/secrets-operator/crds.yaml
